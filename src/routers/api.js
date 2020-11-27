f3cdebf65ed88eb0f54892d6c11db328540c8217
edjo93 <eddas.carrasco@unitec.edu>
2020-11-27 12:58:27 -0600

    prueba push Eddas

--- src/routers/api.js
+++ src/routers/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const administrador = require('./administrador.router');
+const users = require('./usersRoutes');
 
 const api = express.Router();
 
 //
 api.use('/administrador', administrador);
+api.use('/users', users);
 
 module.exports = api;
