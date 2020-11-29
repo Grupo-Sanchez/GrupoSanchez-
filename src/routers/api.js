f5a4a20758a386d00e97d9eb4500ad4f20b211b2
edjo93 <eddas.carrasco@unitec.edu>
2020-11-28 20:00:59 -0600

    funcionalidad de clientes agregada

--- src/routers/api.js
+++ src/routers/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const administrador = require('./administrador.router');
 const users = require('./usersRoutes');
+const clientes = require('./clientesRoutes');
 
 const api = express.Router();
 
 //
 api.use('/administrador', administrador);
 api.use('/users', users);
+api.use('/clientes', clientes);
 
 module.exports = api;
