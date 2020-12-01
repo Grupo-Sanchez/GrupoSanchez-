3212d7da072a146d291f5163842ce88ee946f137
Jean <jeancasoto@yahoo.com>
2020-12-01 01:44:31 -0600

    Agregada la interfaz del formulario (se activa al cargar por primera vez el componente)

---
+++ Frontend/src/Views/AmbienteFormularioCliente.js
@@ -0,0 +1,61 @@
+import React, { useState, useContext, useEffect } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Input,
+  Label,
+  FormGroup,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ButtonGroup,
+} from 'reactstrap';
+
+// Import de ambientes
+import FormCliente from '../Components/FormCliente';
+
+// Import de Forms
+
+// Import de styles
+import '../Styles/FormularioCliente.css';
+
+const AmbienteFormularioCliente = () => {
+  // States
+
+  // Modal
+  const [modalFormularioCliente, setModalFormularioCliente] = useState(false);
+  const toggleFormularioCliente = () => setModalFormularioCliente(!modalFormularioCliente);
+
+  // useEffect
+  useEffect(() => {
+    console.log('Se activó por primera vez AmbienteFormularioCliente');
+    toggleFormularioCliente();
+  }, []);
+
+  return (
+    <Modal
+      size="lg"
+      isOpen={modalFormularioCliente}
+      toggle={toggleFormularioCliente}
+      backdrop="static"
+    >
+      <ModalHeader toggle={toggleFormularioCliente}>
+        <h1 className="FormTitle">Agregar cliente</h1>
+      </ModalHeader>
+      <ModalBody>
+        <FormCliente />
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={toggleFormularioCliente}>
+          Confirmar
+        </Button>{' '}
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default AmbienteFormularioCliente;
