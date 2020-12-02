77ba819e3de5d2fca156c8c7ebddf29d92ff523d
JasserRamos <49545442+JasserRamos@users.noreply.github.com>
2020-12-02 17:39:19 -0600

    FrontEnd Terminado 98%

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,6 @@
-import AmbienteJefeDeTienda from "./Views/AmbienteJefeDeTienda/AmbienteJefeDeTienda.js"
-
-// Importaciones de react
 import React, { useState } from 'react';
+import AmbienteJefeDeTienda from './Views/AmbienteJefeDeTienda/AmbienteJefeDeTienda.jsx';
+// Importaciones de react
 
 const writeSomething = (titulo) => {
   const jsonString = { titulo, terminado: false };
@@ -23,39 +22,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [titulo, setTitulo] = useState('');
 
-  return (
-    <AmbienteJefeDeTienda />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //   </header>
-    // </div>
-    /*<div>
-    <input
-      value={titulo}
-      onChange={(e) => {
-        setTitulo(e.target.value);
-      }}
-    ></input>
-    <button
-      onClick={() => {
-        writeSomething(titulo);
-        fetch("http://localhost:3001/api/administrador")
-          .then((res) => res.json())
-          .then((json) => {
-            setTodos(json.values);
-          });
-      }}
-    >
-      Agregar a mongo
-    </button>
-    <ul>
-      {todos.map((todo) => {
-        return <li key={todo._id}>{todo.titulo}</li>;
-      })}
-    </ul>
-  </div>*/
-  );
+  return <AmbienteJefeDeTienda />;
 }
 
 export default App;
---
+++ Frontend/src/Components/Header.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import '../Styles/Sidebar.css';
+
+const Header = (props) => {
+  const { items } = props;
+
+  const [isOpen, setOpen] = useState(false);
+
+  function handleItemClick(to) {
+    console.log(`Let's go to ${to}!`);
+  }
+
+  return (
+    <nav className="base-header">
+      {/* Header content */}
+      <div className="header-content">
+        {/* Hamburger */}
+        <button className="sidenav-button" onClick={() => setOpen(!isOpen)}>
+          <svg
+            style={{ width: '1em', height: '1em', color: 'white' }}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+
+        <span>Grupo Sanchez</span>
+      </div>
+
+      {isOpen && (
+        <div onClick={() => setOpen(false)} className="sidenav-background">
+          <div onClick={() => setOpen(false)} className="sidenav-background-content"></div>
+        </div>
+      )}
+
+      <aside className={`sidenav ${isOpen ? 'sidenav-opened' : 'sidenav-closed'}`}>
+        <div onClick={() => setOpen(!isOpen)} className="sidenav-header">
+          <svg
+            style={{ width: '1em', height: '1em', cursor: 'pointer', marginRight: '0.5rem' }}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+
+          <span>Grupo Sanchez </span>
+        </div>
+
+        <div className="sidenav-content">
+          {items.map(({ name, to, icon }, i) => (
+            <div key={i} className="sidenav-item" onClick={handleItemClick.bind(this, to)}>
+              {icon}
+              <span>{name}</span>
+            </div>
+          ))}
+        </div>
+
+        <div className="sidenav-footer">Cerrar Sesion</div>
+      </aside>
+    </nav>
+  );
+};
+
+export default Header;
