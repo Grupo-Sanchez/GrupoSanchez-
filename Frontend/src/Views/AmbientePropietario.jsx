98d4a30e17ad2cddd980125b8d268499c7c74e10
Jean <jeancasoto@yahoo.com>
2021-02-10 20:41:52 -0600

    Login a testing

--- Frontend/src/Views/AmbientePropietario.jsx
+++ Frontend/src/Views/AmbientePropietario.jsx
@@ -1,14 +1,14 @@
 import AmbientePropietarioBase from '../Components/AmbientePropietarioBase.jsx';
 import Logo from '../Icons/grupoSanchezLogo.png';
 
-import '../Styles/LoginSignupCard.css';
+// import '../Styles/LoginSignupCard.css';
 
 const AmbientePropietario = () => (
   <AmbientePropietarioBase>
     <div
       style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
     >
-      <img src={Logo} alt="Logo" />
+      {/* <img src={Logo} alt="Logo" /> */}
       {/* Ambiente propietario */}
     </div>
   </AmbientePropietarioBase>
